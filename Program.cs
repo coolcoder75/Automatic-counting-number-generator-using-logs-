509196b4dbcee5df0a9148f5a0b79a1e8c7e2c75
coolcoder75 <mdragon.shock@gmail.com>
2022-07-02 21:36:28 +1000

    2

--- Program.cs
+++ Program.cs
@@ -1,38 +1,21 @@
-﻿using System;
+﻿int[] ArrayF6 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-namespace ConsoleApp1
+int total = ArrayF6.Length;
+
+for (int j = 0; j < total; j++)
 {
-    class Program
+    for (int k = 0; k < total; k++)
     {
-      
-        static void Main(string[] args)
+        for (int l = 0; l < total; l++)
         {
-
-            int[] ArrayF6 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-            int total = ArrayF6.Length;
-
-            for (int j = 0; j < total; j++)
+            if (j > 0)
             {
-                for (int k = 0; k < total; k++)
-                {
-                    for (int l = 0; l < total; l++)
-                    {
-                        if (j > 0)
-                        {
-                            Console.Write(ArrayF6[j]);
-                        }
-                        Console.Write(ArrayF6[k]);
-                        Console.Write(ArrayF6[l] + " ");
-                    }
-                    Console.WriteLine("");
-                }
-                Console.WriteLine("");
+                Console.Write(ArrayF6[j]);
             }
-
-
-            
-        
+            Console.Write(ArrayF6[k]);
+            Console.Write(ArrayF6[l] + " ");
         }
+        Console.WriteLine("");
     }
+    Console.WriteLine("");
 }
